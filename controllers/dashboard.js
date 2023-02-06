f0065f7f96d0af44b56c4072f036a2c128fe2bc3
Glitch (sunset-peaceful-bayberry) <none>
2023-02-06 17:03:28 +0000

    👗🍥 Checkpoint ./controllers/dashboard.js:53489011/1578 ./views/partials/listsongs.hbs:53489011/303

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -4,25 +4,23 @@
 import logger from '../utils/logger.js';
 import playlistStore from '../models/playlist-store.js';
 
-
 // create dashboard object
 const dashboard = {
-  
+
   // index method - responsible for creating and rendering the view
   index(request, response) {
-    
+
     // display confirmation message in log
     logger.info('dashboard rendering');
-    
+
     // create view data object (contains data to be sent to the view e.g. page title)
     const viewData = {
-      
-      playlists: playlistStore.getAllPlaylists(),
-
       title: 'Playlist App Dashboard',
+      playlists: playlistStore.getAllPlaylists(),
     };
-    
+
     // render the dashboard view and pass through the data
+    logger.info('about to render', viewData.playlists);
     response.render('dashboard', viewData);
   },
 };
