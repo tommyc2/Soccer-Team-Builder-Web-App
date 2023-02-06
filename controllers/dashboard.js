5777855991e82e2924fea16258fd83983bacddb0
Glitch (sunset-peaceful-bayberry) <none>
2023-02-06 16:51:34 +0000

    🐩👩‍👩‍👦 Checkpoint ./models/playlist-store.js:53489011/994 ./controllers/dashboard.js:53489011/116

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,6 +2,8 @@
 
 // import all required modules
 import logger from '../utils/logger.js';
+import playlistStore from '../models/playlist-store.js';
+
 
 // create dashboard object
 const dashboard = {
@@ -14,6 +16,9 @@ const dashboard = {
     
     // create view data object (contains data to be sent to the view e.g. page title)
     const viewData = {
+      
+      playlists: playlistStore.getAllPlaylists(),
+
       title: 'Playlist App Dashboard',
     };
     
--- models/playlist-store.js
+++ models/playlist-store.js
@@ -1,11 +1,10 @@
-'use strict';
+"use strict";
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const playlistCollection = require("./playlist-store.json");
 
 const playlistStore = {
-
   // import the playlist collection object
   playlistCollection: playlistCollection.playlistCollection,
 
@@ -13,7 +12,6 @@ const playlistStore = {
   getAllPlaylists() {
     return this.playlistCollection;
   },
-
 };
 
 // export the playlistStore object so it can be used elsewhere
