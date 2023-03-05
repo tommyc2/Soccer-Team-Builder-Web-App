14a7562c9917ae0d7610297a2b5d9dda1ba5c97b
Glitch (sunset-peaceful-bayberry) <none>
2023-03-05 14:05:55 +0000

    🐧🌁 Checkpoint ./models/team-shack.js:53489011/212 ./views/partials/listteams.hbs:53489011/901

--- models/team-shack.js
+++ models/team-shack.js
@@ -5,10 +5,9 @@ const require = createRequire(import.meta.url);
 const teamCollection = require("./team-shack.json");
 
 const teams = {
-  // import the team collection object
   teamsCollection: teamCollection.teamsCollection,
 
-  // the function to get all of the soccer teams
+ 
   getAllTeams() {
     return this.teamsCollection;
   },
@@ -19,14 +18,13 @@ const teams = {
   
   removePlayer(id, playerId) {
     const team = this.getTeam(id);
-    _.remove(team.songs, { id: playerId });
+    _.remove(team.players, { id: playerId });
   },
   
-  removePlaylist(id) {
-    _.remove(this.playlistCollection, { id: id });
+  removeTeam(id) {
+    _.remove(this.teamsCollection, { id: id });
   },
 
 };
 
-// export the playlistStore object so it can be used elsewhere
-export default playlistStore;
+export default teams;
