ced0d706f27e0eabcc145741a9928288c2410a54
Glitch (sunset-peaceful-bayberry) <none>
2023-03-10 17:28:31 +0000

    🗺🔑 Checkpoint ./controllers/dashboard.js:53489011/1308 ./views/partials/addplayer.hbs:53489011/1100

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,27 +1,21 @@
 'use strict';
 
-// import all required modules
 import logger from '../utils/logger.js';
 import teams from '../models/team-shack.js';
 import { v4 as uuidv4 } from 'uuid';
 
-
-// create dashboard object
 const dashboard = {
 
-  // index method - responsible for creating and rendering the view
+  
   index(request, response) {
 
-    // display confirmation message in log
     logger.info('dashboard rendering');
 
-    // create view data object (contains data to be sent to the view e.g. page title)
     const viewData = {
       title: 'Soccer Teams',
       teams: teams.getAllTeams(),
     };
-
-    // render the dashboard view and pass through the data
+    
     logger.info('about to render', viewData.teams);
     response.render('dashboard', viewData);
   },
@@ -45,5 +39,5 @@ const dashboard = {
   
 };
 
-// export the dashboard module
+
 export default dashboard;
