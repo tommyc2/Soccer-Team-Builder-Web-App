0cc260fc12f7b512c37c272a2a2d08ea24873b3a
Glitch (sunset-peaceful-bayberry) <none>
2023-03-30 15:11:42 +0000

    🚎🚖 Checkpoint ./server.js:53489011/135 ./models/user-store.json:53489011/390 ./models/user-store.js:53489011/606 ./views/index.hbs:53489011/122 ./views/login.hbs:53489011/459 ./views/signup.hbs:53489011/830 ./views/partials/welcomemenu.hbs:53489011/362 ./views/partials/menu.hbs:53489011/824 ./public/style.css:53489011/278 ./controllers/accounts.js:53489011/2270

---
+++ controllers/accounts.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import logger from '../utils/logger.js';
+import userStore from '../models/user-store.js';
+import { v4 as uuidv4 } from 'uuid';
+
+const accounts = {
+
+
+  index(request, response) {
+    const viewData = {
+      title: 'Login or Signup',
+    };
+    response.render('index', viewData);
+  },
+
+  login(request, response) {
+    const viewData = {
+      title: 'Login to the Service',
+    };
+    response.render('login', viewData);
+  },
+
+
+  logout(request, response) {
+    response.cookie('team', '');
+    response.redirect('/');
+  },
+
+
+  signup(request, response) {
+    const viewData = {
+      title: 'Login to the Service',
+    };
+    response.render('signup', viewData);
+  },
+
+  register(request, response) {
+    const user = request.body;
+    user.id = uuidv4();
+    userStore.addUser(user);
+    logger.info('registering' + user.email);
+    response.redirect('/');
+  },
+
+
+  authenticate(request, response) {
+    const user = userStore.getUserByEmail(request.body.email);
+    if (user) {
+      response.cookie('team', user.email);
+      logger.info('logging in' + user.email);
+      response.redirect('/start');
+    } else {
+      response.redirect('/login');
+    }
+  },
+
+  getCurrentUser (request) {
+    const userEmail = request.cookies.team;
+    return userStore.getUserByEmail(userEmail);
+  }
+}
+
+export default accounts;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// use javascript in strict mode
+
 "use strict";
 
 
@@ -15,16 +15,16 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(cookieParser());
 
-// use handlebars as view engine
+
 const handlebars = exphbs.create({ extname: ".hbs" });
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
-// import routes file and use this for routing
+
 import routes from "./routes.js";
 app.use("/", routes);
 
-// listen for requests :)
+
 const listener = app.listen(process.env.PORT || 4000, function () {
   logger.info("Your app is listening on port " + listener.address().port);
 });
